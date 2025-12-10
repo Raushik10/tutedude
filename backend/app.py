156023338ec8c7b9e1dde355afa7ecc1746628ca
Rahul Kumar <rahulkumar100406@gmail.com>
2025-12-10 15:50:52 +0530

    app.py updated

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 from flask import Flask
 from flask import request, jsonify, send_from_directory
 from pymongo import MongoClient
-=======
-from flask import request, jsonify
->>>>>>> master_2
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -20,18 +16,12 @@ MONGO_URI = os.getenv('MONGO_URI')
 # Create a new client and connect to the server
 client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 
-<<<<<<< HEAD
-
-=======
-# Send a ping to confirm a successful connection
->>>>>>> master_2
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
-<<<<<<< HEAD
 db = client["test_db"]
 test_col = db["items"]
 
@@ -46,11 +36,6 @@ def serve_home():
 
 
 
-=======
-db = client["Test_db"]
-test_col = db["items"]
-
->>>>>>> master_2
 @app.route("/submittodoitem", methods=["POST"])
 def submit_todo_item():
     data = request.get_json() or {}
@@ -60,11 +45,8 @@ def submit_todo_item():
         return jsonify({"error":"itemName required"}), 400
     res = todo_col.insert_one({"itemName":name,"itemDescription":desc})
     return jsonify({"inserted_id": str(res.inserted_id)}), 201
-<<<<<<< HEAD
 
 
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0',port=7000,debug=True)
-=======
->>>>>>> master_2
